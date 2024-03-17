ceadee902b15b7a00c3ca3fa561d95a9e092d301
makinosp <makinosp@icloud.com>
2024-03-17 12:44:54 +0900

    update: friend, user model

---
+++ Sources/VRCKit/Models/Friends/FriendDetailModel.swift
@@ -0,0 +1,31 @@
+//
+//  FriendDetailModel.swift
+//  
+//
+//  Created by makinosp on 2024/03/17.
+//
+
+import Foundation
+
+public struct FriendDetail: Codable, FriendDetailRepresentable {
+    public let bio: String
+    public let bioLinks: [String]?
+    public let currentAvatarImageUrl: String
+    public let currentAvatarThumbnailImageUrl: String
+    public let developerType: String
+    public let displayName: String
+    public let id: String
+    public let isFriend: Bool
+    public let lastLogin: Date
+    public let lastPlatform: String
+    public let profilePicOverride: String
+    public let status: String
+    public let statusDescription: String
+    public let tags: [String]
+    public let userIcon: String
+    public let location: String
+    public let friendKey: String
+    public let dateJoined: String
+    public var note: String
+    public let lastActivity: Date
+}
