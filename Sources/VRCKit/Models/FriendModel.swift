a07f6ae458c03f6e55141b5743384d02fff37e3b
makinosp <makinosp@icloud.com>
2024-04-14 13:53:31 +0900

    feat: enumerate user status

--- Sources/VRCKit/Models/FriendModel.swift
+++ Sources/VRCKit/Models/FriendModel.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+public enum Status: String, Codable, CaseIterable, Identifiable {
+    case joinMe = "join me"
+    case active
+    case askMe = "ask me"
+    case busy
+    case offline
+
+    public var id: Int {
+        self.hashValue
+    }
+}
+
 public struct Friend: Codable, ProfileElementRepresentable {
     public let bio: String?
     public let bioLinks: [String]?
@@ -19,7 +31,7 @@ public struct Friend: Codable, ProfileElementRepresentable {
     public let lastLogin: Date
     public let lastPlatform: String
     public let profilePicOverride: String
-    public let status: String
+    public let status: Status
     public let statusDescription: String
     public let tags: [String]
     public let userIcon: String
