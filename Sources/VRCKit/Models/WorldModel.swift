585613a64bc0548c4adcfc878d4147dfe36f4a87
makinosp <makinosp@icloud.com>
2024-06-02 21:41:22 +0900

    feat: connect world API

--- Sources/VRCKit/Models/WorldModel.swift
+++ Sources/VRCKit/Models/WorldModel.swift
@@ -5,35 +5,65 @@
 //  Created by makinosp on 2024/02/12.
 //
 
-public struct World: Codable, Identifiable, Hashable {
-    public let id: String?
-    public let name: String?
-    public let description: String?
-    public let featured: Bool?
-    public let authorId: String?
-    public let authorName: String?
+public struct World: Identifiable, Hashable {
+    public let id: String
+    public let name: String
+    public let description: String
+    public let featured: Bool
+    public let authorId: String
+    public let authorName: String
 
-    public let capacity: Int?
-    public let tags: [String]?
-    public let releaseStatus: String?
-    public let imageUrl: String?
-    public let thumbnailImageUrl: String?
+    public let capacity: Int
+    public let tags: [String]
+    public let releaseStatus: String
+    public let imageUrl: String
+    public let thumbnailImageUrl: String
     public let namespace: String?
 
     public let version: Int?
 
-    public let organization: String?
+    public let organization: String
     public let previewYoutubeId: String?
-    public let favorites: Int?
-    public let created_at: String?
-    public let updated_at: String?
-    public let publicationDate: String?
-    public let labsPublicationDate: String?
-    public let visits: Int?
-    public let popularity: Int?
-    public let heat: Int?
-    public let publicOccupants: Int?
-    public let privateOccupants: Int?
+    public let favorites: Int
+    public let createdAt: String
+    public let updatedAt: String
+    public let publicationDate: String
+    public let labsPublicationDate: String
+    public let visits: Int
+    public let popularity: Int
+    public let heat: Int
+    public let publicOccupants: Int
+    public let privateOccupants: Int
 
-    //    public let instances: [(Any)]?
+    // public let instances: [(Any)]?
+}
+
+extension World: Codable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case description
+        case featured
+        case authorId
+        case authorName
+        case capacity
+        case tags
+        case releaseStatus
+        case imageUrl
+        case thumbnailImageUrl
+        case namespace
+        case version
+        case organization
+        case previewYoutubeId
+        case favorites
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+        case publicationDate
+        case labsPublicationDate
+        case visits
+        case popularity
+        case heat
+        case publicOccupants
+        case privateOccupants
+    }
 }
