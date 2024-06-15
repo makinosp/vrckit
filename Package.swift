afcec239f867cdec9f2f09c615961dbdfd893144
makinosp <makinosp@icloud.com>
2024-06-15 14:37:23 +0900

    feat: update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -30,3 +30,9 @@ let package = Package(
         )
     ]
 )
+
+package.targets.forEach {
+    $0.swiftSettings = [
+        .enableUpcomingFeature("ForwardTrailingClosures"), // SE-0286
+    ]
+}
