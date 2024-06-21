af44609d3eb773ad43653723ded5b8805dc0d6bd
makinosp <makinosp@icloud.com>
2024-06-21 23:05:31 +0900

    refactor: friend service

--- Sources/VRCKit/Errors.swift
+++ Sources/VRCKit/Errors.swift
@@ -10,18 +10,21 @@ import Foundation
 public enum VRCKitError: Error, LocalizedError {
     public typealias RawValue = String
 
-    case encodingError
     case apiError(_ details: String)
+    case clientDeallocated
+    case encodingError
     case invalidResponseError
     case invalidRequest(_ details: String)
     case unexpectedError
 
     public var errorDescription: String? {
         return switch self {
-        case .encodingError:
-            "Encoding Error"
         case .apiError(let _):
             "API Error"
+        case .encodingError:
+            "Encoding Error"
+        case .clientDeallocated:
+            "Client Deallocated"
         case .invalidResponseError:
             "Invalid Response Error"
         case .invalidRequest:
