2cc855cc290695494c8d02b9b349feb0b14c6d1e
makinosp <makinosp@icloud.com>
2024-07-10 23:37:30 +0900

    fix: dateformatter format string

--- Sources/VRCKit/Extensions/DateFormatter+.swift
+++ Sources/VRCKit/Extensions/DateFormatter+.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension DateFormatter {
     static let iso8601Full: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
         formatter.calendar = Calendar(identifier: .iso8601)
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.locale = Locale(identifier: "en_US_POSIX")
