9c7a5d96992a0d0c265fb8f6005cb14a0ad2a3d8
makinosp <makinosp@icloud.com>
2024-07-10 23:29:04 +0900

    feat: custom datestring serializer

---
+++ Sources/VRCKit/Utils/OptionalISO8601Date.swift
@@ -0,0 +1,39 @@
+//
+//  OptionalISO8601Date.swift
+//  
+//
+//  Created by makinosp on 2024/07/10.
+//
+
+import Foundation
+
+public struct OptionalISO8601Date: Codable, Hashable {
+    private let date: Date?
+    private let formatter = DateFormatter.iso8601Full
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let dateString = try container.decode(String.self)
+
+        if dateString == "none" || dateString.isEmpty {
+            self.date = nil
+        } else if let date = formatter.date(from: dateString) {
+            self.date = date
+        } else {
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Invalid date format"
+            )
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        if let date = self.date {
+            let dateString = formatter.string(from: date)
+            try container.encode(dateString)
+        } else {
+            try container.encode("none")
+        }
+    }
+}
