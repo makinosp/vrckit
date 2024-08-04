765e85b66b122b54089b7fcb704dac99f5896a49
makinosp <makinosp@icloud.com>
2024-08-04 13:25:44 +0900

    feat: implement trust rank utility

---
+++ Sources/VRCKit/Utils/TrustRank.swift
@@ -0,0 +1,43 @@
+//
+//  TrustRank.swift
+//  VRCKit
+//
+//  Created by makinosp on 2024/08/04.
+//
+
+public enum TrustRank {
+    case trusted, known, user, newUser, visitor, unknown
+}
+
+public extension ProfileElementRepresentable {
+    var trustRank: TrustRank {
+        let rankTags: [SystemTag] = [
+            .systemTrustVeteran,
+            .systemTrustTrusted,
+            .systemTrustKnown,
+            .systemTrustBasic
+        ]
+        let rankTag = rankTags.first(where: { tags.systemTags.contains($0) })
+        guard let rankTag = rankTag else { return .visitor }
+        return switch rankTag {
+        case .systemTrustVeteran: .trusted
+        case .systemTrustTrusted: .known
+        case .systemTrustKnown: .user
+        case .systemTrustBasic: .newUser
+        default: .unknown
+        }
+    }
+}
+
+extension TrustRank: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .trusted: "Trusted"
+        case .known: "Known"
+        case .user: "User"
+        case .newUser: "New User"
+        case .visitor: "Visitor"
+        case .unknown: "Unknown"
+        }
+    }
+}
