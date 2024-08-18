afd77c09a35cf121d3f6a5954261dec176448a47
makinosp <makinosp@icloud.com>
2024-08-18 11:07:10 +0900

    feat: update thumbnail url representation

--- Sources/VRCKit/Utils/ProfileImageURL.swift
+++ Sources/VRCKit/Utils/ProfileImageURL.swift
@@ -9,20 +9,12 @@ import Foundation
 
 public extension ProfileElementRepresentable {
     var thumbnailUrl: URL? {
-        if let userIcon = userIcon {
-            return userIcon
+        if let userIcon = userIcon { return userIcon }
+        guard let url = avatarThumbnailUrl else { return nil }
+        var urlString = url.absoluteString
+        if urlString.hasSuffix("256") {
+            urlString = String(urlString.dropLast(3)) + "1024"
         }
-        guard let url = avatarThumbnailUrl,
-              let urlString = url.absoluteString.hasSuffix("256")
-                ? String(url.absoluteString.dropLast(3)) + "512"
-                : avatarImageUrl?.absoluteString else { return nil }
         return URL(string: urlString)
-    }
-
-    var userIconUrl: URL? {
-        if let userIcon = userIcon {
-            return userIcon
-        }
-        return avatarThumbnailUrl
     }
 }
