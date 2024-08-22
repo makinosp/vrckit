885b073938497b9b068949b6ecb552708c9d5fd5
makinosp <makinosp@icloud.com>
2024-08-22 21:28:32 +0900

    feat: implement description of language tags

--- Sources/VRCKit/Models/TagModel.swift
+++ Sources/VRCKit/Models/TagModel.swift
@@ -76,6 +76,53 @@ public enum LanguageTag: String, Hashable, Codable {
     case noLinguisticContent = "language_zxx"
 }
 
+extension LanguageTag: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .english: "English"
+        case .korean: "한국어"
+        case .russian: "Русский"
+        case .spanish: "Español"
+        case .portuguese: "Português"
+        case .chinese: "中文"
+        case .german: "Deutsch"
+        case .japanese: "日本語"
+        case .french: "Français"
+        case .swedish: "Svenska"
+        case .dutch: "Nederlands"
+        case .polish: "Polski"
+        case .danish: "Dansk"
+        case .norwegian: "Norsk"
+        case .italian: "Italiano"
+        case .thai: "ภาษาไทย"
+        case .finnish: "Suomi"
+        case .hungarian: "Magyar"
+        case .czech: "Čeština"
+        case .turkish: "Türkçe"
+        case .arabic: "العربية"
+        case .romanian: "Română"
+        case .vietnamese: "Tiếng Việt"
+        case .bahasaIndonesia: "Bahasa Indonesia"
+        case .bahasaMelayu: "Bahasa Melayu"
+        case .filipino: "Filipino"
+        case .mandarinChinese: "官话"
+        case .hebrew: "עברית"
+        case .hmong: "Hmoob"
+        case .ukrainian: "украї́нська"
+        case .tokiPona: "toki pona"
+        case .yueChinese: "廣東話"
+        case .wuChinese: "吳語"
+        case .americanSignLanguage: "American Sign Language"
+        case .britishSignLanguage: "British Sign Language"
+        case .dutchSignLanguage: "Dutch Sign Language"
+        case .frenchSignLanguage: "French Sign Language"
+        case .japaneseSignLanguage: "日本手話"
+        case .koreanSignLanguage: "한국 수화 언어"
+        case .noLinguisticContent: "No linguistic content"
+        }
+    }
+}
+
 extension SystemTag: Identifiable {
     public var id: Int { hashValue }
 }
