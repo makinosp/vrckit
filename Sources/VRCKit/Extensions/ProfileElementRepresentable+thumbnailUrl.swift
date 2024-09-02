029cba025b9f9570b0febaa4bfd844823fea9a7b
makinosp <makinosp@icloud.com>
2024-09-02 18:56:15 +0900

    feat: implement a function to replace the resolution of image URLs

--- Sources/VRCKit/Extensions/ProfileElementRepresentable+thumbnailUrl.swift
+++ Sources/VRCKit/Extensions/ProfileElementRepresentable+thumbnailUrl.swift
@@ -8,12 +8,16 @@
 import Foundation
 
 public extension ProfileElementRepresentable {
-    var thumbnailUrl: URL? {
-        if let userIcon = userIcon { return userIcon }
-        guard let url = avatarThumbnailUrl else { return nil }
+    func imageUrl(_ resolution: ImageResolution) -> URL? {
+        guard let url = userIcon ?? avatarThumbnailUrl else { return nil }
+        return replaceImageUrl(url: url, resolution: resolution)
+    }
+
+    private func replaceImageUrl(url: URL, resolution: ImageResolution) -> URL? {
+        guard resolution != .origin, Int(url.lastPathComponent) != nil else { return url }
         var urlString = url.absoluteString
-        if urlString.hasSuffix("256") {
-            urlString = String(urlString.dropLast(3)) + "1024"
+        if let range = urlString.range(of: url.lastPathComponent, options: .backwards) {
+            urlString.replaceSubrange(range, with: resolution.description)
         }
         return URL(string: urlString)
     }
---
+++ Sources/VRCKit/Models/ImageResolution.swift
@@ -0,0 +1,16 @@
+//
+//  ImageResolution.swift
+//  VRCKit
+//
+//  Created by makinosp on 2024/09/02.
+//
+
+public enum ImageResolution: Int, CustomStringConvertible {
+    case x256 = 256
+    case x1024 = 1024
+    case origin = 0
+
+    public var description: String {
+        rawValue.description
+    }
+}
