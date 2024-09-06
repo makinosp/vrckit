d9706b08eb8699b70f5b0846a7a2a263406d0806
makinosp <makinosp@icloud.com>
2024-09-06 21:18:39 +0900

    fix: replacing image url

--- Sources/VRCKit/Protocols/ImageUrlRepresentableProtocol.swift
+++ Sources/VRCKit/Protocols/ImageUrlRepresentableProtocol.swift
@@ -13,9 +13,9 @@ public protocol ImageUrlRepresentable {
 
 public extension ImageUrlRepresentable {
     func replaceImageUrl(url: URL, resolution: ImageResolution) -> URL? {
-        guard resolution != .origin, Int(url.lastPathComponent) != nil else { return url }
+        guard resolution != .origin, let number = Int(url.lastPathComponent), number > 1 else { return url }
         var urlString = url.absoluteString
-        if let range = urlString.range(of: url.lastPathComponent, options: .backwards) {
+        if let range = urlString.range(of: number.description, options: .backwards) {
             urlString.replaceSubrange(range, with: resolution.description)
         }
         return URL(string: urlString)
