c5e0c88a1234c6f720a46e24b8aef2f077f21c78
makinosp <makinosp@icloud.com>
2024-09-15 14:27:55 +0900

    update: remove unnecessary dependency

--- Package.swift
+++ Package.swift
@@ -12,23 +12,14 @@ let package = Package(
         .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VRCKit",
             targets: ["VRCKit"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.55.1")
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VRCKit",
-            dependencies: [],
             path: "Sources"
         ),
         .testTarget(
@@ -37,12 +28,3 @@ let package = Package(
         )
     ]
 )
-
-package.targets.forEach {
-    $0.plugins = [
-        .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
-    ]
-    $0.swiftSettings = [
-        .enableUpcomingFeature("ForwardTrailingClosures") // SE-0286
-    ]
-}
