fd11def5b33eb59fb56823dd4ac2f6cc0c39f637
makinosp <makinosp@icloud.com>
2024-09-26 15:30:41 +0900

    refact: add doc in UserNoteService

--- Sources/VRCKit/Services/UserNoteService.swift
+++ Sources/VRCKit/Services/UserNoteService.swift
@@ -9,11 +9,16 @@ public final actor UserNoteService: APIService, UserNoteServiceProtocol {
     let client: APIClient
     private let path = "userNotes"
 
+    // Initializes the AuthenticationService with an APIClient instance
     public init(client: APIClient) {
         self.client = client
     }
 
-    /// Update user's note
+    /// Updates the note for a specific user by sending the note to the API.
+    /// - Parameters:
+    ///   - targetUserId: The ID of the user for whom the note is being updated.
+    ///   - note: The content of the note to be added or updated.
+    /// - Returns: A `UserNoteResponse` containing the updated note information.
     public func updateUserNote(
         targetUserId: String,
         note: String
@@ -24,10 +29,15 @@ public final actor UserNoteService: APIService, UserNoteServiceProtocol {
         return try await Serializer.shared.decode(response.data)
     }
 
+    /// Clears the note for a specific user by sending an empty note to the API.
+    /// - Parameter targetUserId: The ID of the user whose note is being cleared.
     public func clearUserNote(targetUserId: String) async throws {
         _ = try await request(userNote: UserNoteRequest(targetUserId: targetUserId, note: ""))
     }
 
+    /// Sends a request to update or clear a user note.
+    /// - Parameter userNote: The `UserNoteRequest` containing the user ID and note content.
+    /// - Returns: The `HTTPResponse` received from the API.
     private func request(userNote: UserNoteRequest) async throws -> APIClient.HTTPResponse {
         let requestData = try await Serializer.shared.encode(userNote)
         return try await client.request(path: path, method: .post, body: requestData)
