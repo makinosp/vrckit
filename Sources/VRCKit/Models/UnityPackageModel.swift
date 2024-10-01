f886907a485143b1680abd8a7ba71650fd71441e
makinosp <makinosp@icloud.com>
2024-10-01 21:55:02 +0900

    feat: make createdAt in UnityPackage safe decodable

--- Sources/VRCKit/Models/UnityPackageModel.swift
+++ Sources/VRCKit/Models/UnityPackageModel.swift
@@ -9,16 +9,29 @@ import Foundation
 import MemberwiseInit
 
 @MemberwiseInit(.public)
-public struct UnityPackage: Codable, Sendable, Identifiable, Hashable {
+public struct UnityPackage: Sendable, Identifiable, Hashable {
     public let id: String
     @InitWrapper(type: SafeDecoding<URL>) @SafeDecoding public var assetUrl: URL?
     public let assetVersion: Int
-    public let createdAt: Date
+    public let createdAt: OptionalISO8601Date
     public let platform: Platform
     public let unitySortNumber: Int
     public let unityVersion: String
 
     public enum Platform: String, Codable, Sendable {
         case android, ios, standalonewindows
+    }
+}
+
+extension UnityPackage: Codable {
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(String.self, forKey: .id)
+        _assetUrl = try container.decode(SafeDecoding<URL>.self, forKey: .assetUrl)
+        assetVersion = try container.decode(Int.self, forKey: .assetVersion)
+        createdAt = try container.decodeIfPresent(OptionalISO8601Date.self, forKey: .createdAt) ?? OptionalISO8601Date()
+        platform = try container.decode(UnityPackage.Platform.self, forKey: .platform)
+        unitySortNumber = try container.decode(Int.self, forKey: .unitySortNumber)
+        unityVersion = try container.decode(String.self, forKey: .unityVersion)
     }
 }
