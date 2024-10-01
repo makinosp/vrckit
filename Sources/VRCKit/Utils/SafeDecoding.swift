232368eccb6963430759ad5cf3466527f715baf6
makinosp <makinosp@icloud.com>
2024-10-01 21:14:17 +0900

    feat: refine SafeDecoding property wrapper

--- Sources/VRCKit/Utils/SafeDecoding.swift
+++ Sources/VRCKit/Utils/SafeDecoding.swift
@@ -5,19 +5,33 @@
 //  Created by makinosp on 2024/09/30.
 //
 
-import Foundation
-
 @propertyWrapper
-public struct SafeDecoding<T>: Codable, Sendable where T: Codable & Sendable {
+public struct SafeDecoding<T> {
     public var wrappedValue: T?
 
+    public init(wrappedValue: T? = nil) {
+        self.wrappedValue = wrappedValue
+    }
+}
+
+extension SafeDecoding: Decodable where T: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         wrappedValue = try? container.decode(T.self)
     }
+}
 
+extension SafeDecoding: Encodable where T: Encodable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         try container.encode(wrappedValue)
     }
 }
+
+extension SafeDecoding: Identifiable where T: Identifiable {
+    public var id: T.ID? { wrappedValue?.id }
+}
+
+extension SafeDecoding: Equatable where T: Equatable {}
+extension SafeDecoding: Hashable where T: Hashable {}
+extension SafeDecoding: Sendable where T: Sendable {}
