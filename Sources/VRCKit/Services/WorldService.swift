fd6c91b3619b88a4f5c1877075b2c3944e0b7c00
makinosp <makinosp@icloud.com>
2024-10-16 22:38:02 +0900

    feat: fetching all favorite worlds in WorldService

--- Sources/VRCKit/Services/WorldService.swift
+++ Sources/VRCKit/Services/WorldService.swift
@@ -12,14 +12,30 @@ import MemberwiseInit
 public final actor WorldService: APIService, WorldServiceProtocol {
     public let client: APIClient
     private let path = "worlds"
+    private let limit = 100
 
     public func fetchWorld(worldId: String) async throws -> World {
         let response = try await client.request(path: "\(path)/\(worldId)", method: .get)
         return try await Serializer.shared.decode(response.data)
     }
 
-    public func fetchFavoritedWorlds(n: Int = 100) async throws -> [FavoriteWorld] {
-        let queryItems = [URLQueryItem(name: "n", value: n.description)]
+    public func fetchFavoritedWorlds() async throws -> [FavoriteWorld] {
+        var allFavorites = Set<FavoriteWorld>()
+        var offset = 0
+        while true {
+            let batch = try await fetchFavoritedWorlds(n: limit, offset: offset)
+            allFavorites.formUnion(batch)
+            if batch.count < limit { break }
+            offset += limit
+        }
+        return Array(allFavorites)
+    }
+
+    private func fetchFavoritedWorlds(n: Int, offset: Int) async throws -> [FavoriteWorld] {
+        let queryItems = [
+            URLQueryItem(name: "n", value: n.description),
+            URLQueryItem(name: "offset", value: offset.description)
+        ]
         let response = try await client.request(path: "\(path)/favorites", method: .get, queryItems: queryItems)
         let favoriteWorldWrapper: SafeDecodingArray<FavoriteWorld> = try await Serializer.shared.decode(response.data)
         return favoriteWorldWrapper.wrappedValue
