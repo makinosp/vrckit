7a8f64e9bb19130a46ed84a06cd938fcbf4eb29c
makinosp <makinosp@icloud.com>
2024-11-02 22:02:09 +0900

    refact: update doc of UserNoteServiceProtocol

--- Sources/VRCKit/Services/UserNoteService.swift
+++ Sources/VRCKit/Services/UserNoteService.swift
@@ -12,11 +12,6 @@ public final actor UserNoteService: APIService, UserNoteServiceProtocol {
     public let client: APIClient
     private let path = "userNotes"
 
-    /// Updates the note for a specific user by sending the note to the API.
-    /// - Parameters:
-    ///   - targetUserId: The ID of the user for whom the note is being updated.
-    ///   - note: The content of the note to be added or updated.
-    /// - Returns: A `UserNoteResponse` containing the updated note information.
     public func updateUserNote(
         targetUserId: String,
         note: String
@@ -27,8 +22,6 @@ public final actor UserNoteService: APIService, UserNoteServiceProtocol {
         return try Serializer.shared.decode(response.data)
     }
 
-    /// Clears the note for a specific user by sending an empty note to the API.
-    /// - Parameter targetUserId: The ID of the user whose note is being cleared.
     public func clearUserNote(targetUserId: String) async throws {
         _ = try await request(userNote: UserNoteRequest(targetUserId: targetUserId, note: ""))
     }
