44d2e0233558efc505a3ea64d693ce20da561254
makinosp <makinosp@icloud.com>
2024-11-04 21:30:20 +0900

    refact: lint

--- Sources/VRCKit/Services/AuthenticationService.swift
+++ Sources/VRCKit/Services/AuthenticationService.swift
@@ -30,7 +30,7 @@ public final actor AuthenticationService: APIService, AuthenticationServiceProto
         } catch _ as DecodingError {
             let result: RequiresTwoFactorAuthResponse = try Serializer.shared.decode(response.data)
             guard let requires = result.requires else {
-                throw VRCKitError.invalidResponse("\(result.requires?.rawValue)")
+                throw VRCKitError.invalidResponse(String(describing: result.requires?.rawValue))
             }
             return .right(requires)
         }
